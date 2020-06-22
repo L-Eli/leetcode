c503ecf0f9f6b62cd7c9556c3104070368c6feb5
Eli Lin <lineli@garena.com>
2020-06-22 11:41:41 +0800

    00050: pow(x, n) - 2

--- algorithms/js/00050-powx-n.js
+++ algorithms/js/00050-powx-n.js
@@ -3,6 +3,23 @@
  * @param {number} n
  * @return {number}
  */
-var myPow = function(x, n) {
-    return Math.pow(x, n);
+var myPow = function (x, n) {
+    if (n == 0) {
+        return 1;
+    }
+
+    if (n == 1) {
+        return x;
+    }
+
+    let result = myPow(x, parseInt(n / 2))
+    if (n % 2 == 0) {
+        return result * result;
+    }
+
+    if (n > 0) {
+        return x * result * result;
+    }
+
+    return result * result / x;
 };
