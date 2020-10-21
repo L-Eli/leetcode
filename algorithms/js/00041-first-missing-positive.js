5e6afb627abd7b65964b9324065b449e78d2e3b3
Eli Lin <lineli@garena.com>
2020-10-21 14:07:25 +0800

    00041: first missing positive

---
+++ algorithms/js/00041-first-missing-positive.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function (nums) {
+    let i = 0;
+    while (i < nums.length) {
+        if (nums[i] <= nums.length && nums[i] > 0 && nums[i] !== nums[nums[i] - 1]) {
+            let temp = nums[nums[i] - 1];
+            nums[nums[i] - 1] = nums[i];
+            nums[i] = temp;
+            continue;
+        }
+        i++;
+    }
+
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+
+    return nums.length + 1;
+};
